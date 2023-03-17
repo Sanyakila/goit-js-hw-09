910a0cc696cee96d2fec375ebba9c0161464e516
Oleksandr Kivalo <113942764+Sanyakila@users.noreply.github.com>
2023-03-17 19:30:57 +0200

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  btnStop.setAttribute('disabled', '');
+
+  btnStart.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+  });
+
+  btnStop.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    btnStart.removeAttribute('disabled');
+
+    clearInterval(intervalId);
+  });
